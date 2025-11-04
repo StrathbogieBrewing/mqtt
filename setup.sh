677ec0d75f3c1ec17a2d5e92f67d98b290c6205b
johny hoops <johnyhoops@gmail.com>
2025-11-04 14:41:31 +1100

    use absolute path in caddyfile

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ apt install caddy podman certbot
 
 caddy start
 
+echo 'net.ipv4.ip_unprivileged_port_start=80' > /etc/sysctl.conf
+sysctl -p
 
 podman run -it -p 1883:1883 -v "$PWD/mosquitto/config:/mosquitto/config" -v /mosquitto/data -v /mosquitto/log docker.io/eclipse-mosquitto
