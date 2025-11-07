74f4dca9d60298ebdc90633862a672d520240738
johny hoops <johnyhoops@gmail.com>
2025-11-07 14:09:02 +1100

    wiregaurd and mosquitto

--- setup.sh
+++ setup.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-apt install caddy podman certbot
+apt update
+apt install -y podman podman-compose git wireguard ufw
 
-caddy start
+ufw allow 22/tcp
+ufw allow 51820/udp
 
-echo 'net.ipv4.ip_unprivileged_port_start=80' > /etc/sysctl.conf
-sysctl -p
+mkdir -p /etc/wireguard/keys
+cd /etc/wireguard/keys
+umask 077
+wg genkey | tee privatekey | wg pubkey > publickey
 
-podman run -it -p 1883:1883 -v "$PWD/mosquitto/config:/mosquitto/config" -v /mosquitto/data -v /mosquitto/log docker.io/eclipse-mosquitto
+# mkdir /etc/wiregaurd/
+cp wiregaurd.conf /etc/wiregaurd/
+
+systemctl start wg-quick@wg0.service
+
+useradd podman
+
+
+
+
+
+
+
+# caddy start
+
+# echo 'net.ipv4.ip_unprivileged_port_start=80' > /etc/sysctl.conf
+# sysctl -p
+
+# podman run -it -p 1883:1883 -v "$PWD/mosquitto/config:/mosquitto/config" -v /mosquitto/data -v /mosquitto/log docker.io/eclipse-mosquitto
